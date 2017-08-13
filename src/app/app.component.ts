85c8f4ea1cd8dbd44c1ae98917d393b89937b04f
mohamedtwice <mohamedtwice@gmail.com>
2017-08-13 17:39:22 -0500

    base project

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { TabsPage } from '../pages/tabs/tabs';
+import { Login } from '../pages/login/login';
+import { HomePage } from '../pages/home/home';
+
+import firebase from 'firebase';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = TabsPage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+      // Initialize Firebase
+      var config = {
+      apiKey: "AIzaSyBknsVaC3e0hOfE7miBWTSPRLUYOOHZHGw",
+authDomain: "orders-74065.firebaseapp.com",
+databaseURL: "https://orders-74065.firebaseio.com",
+projectId: "orders-74065",
+storageBucket: "orders-74065.appspot.com",
+messagingSenderId: "727831329242"
+      };
+      firebase.initializeApp(config);
+      firebase.auth().onAuthStateChanged((user) => {
+
+          if (!user) {
+              console.log("not login");
+              this.rootPage = Login;
+
+
+          } else {
+              console.log("login");
+              this.rootPage = HomePage;
+
+          }
+
+      });
+
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+}
